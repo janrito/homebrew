f71b464189717adf48bc55067ddd540bd971fe25
Alejandro Giacometti <alejandro.giacometti@gmail.com>
2013-12-14 14:20:06 +0000

    patched valgrind 3.10.0, temporary fix

--- Library/Formula/valgrind.rb
+++ Library/Formula/valgrind.rb
@@ -20,14 +20,8 @@ class Valgrind < Formula
   skip_clean 'lib/valgrind'
 
   def patches
-    # 1: For Xcode-only systems, we have to patch hard-coded paths, use xcrun &
-    #    add missing CFLAGS. See: https://bugs.kde.org/show_bug.cgi?id=295084
-    # 2: Fix for 10.7.4 w/XCode-4.5, duplicate symbols. Reported upstream in
-    #    https://bugs.kde.org/show_bug.cgi?id=307415
-    p = []
-    p << 'https://gist.github.com/raw/3784836/f046191e72445a2fc8491cb6aeeabe84517687d9/patch1.diff' unless MacOS::CLT.installed?
-    p << 'https://gist.github.com/raw/3784930/dc8473c0ac5274f6b7d2eb23ce53d16bd0e2993a/patch2.diff' if MacOS.version == :lion
-    return p
+    # v4 of patch for Mavericks support, cf. https://bugs.kde.org/show_bug.cgi?id=326724
+    { :p0 => 'http://bugsfiles.kde.org/attachment.cgi?id=83590' }
   end
 
   def install
